430a86743d7be5d0324b578736e8a5a1ef986231
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-03-19 13:49:45 +0200

    korpplugins: Add protectedcorporadb
    Add korpplugins/protectedcorporadb: A plugin that retrieves a list of
    protected corpora from a MySQL database.

---
+++ korpplugins/protectedcorporadb/info.py
@@ -0,0 +1,12 @@
+"""
+korpplugins.protectedcorporadb.info
+
+Information on the protectedcorporadb Korp plugin.
+"""
+
+
+NAME = "Protected corpora database"
+VERSION = "0.1"
+DATE = "2021-03-16"
+AUTHOR = "Jyrki Niemi / FIN-CLARIN"
+DESCRIPTION = "Retrieve a list of protected corpora from a MySQL database."
