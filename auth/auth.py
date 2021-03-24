2dd165f5d98bb8fecb4c61481274016c9b9bbc00
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-03-24 16:33:19 +0200

    auth.py: Convert to a Flask WSGI application
    auth/auth.py:
    - Convert to a Flask WSGI application.
    - Slightly restructure and reformat code.
    - Remove support for HTTP Basic authentication.
    - Get configuration information from config module.
    - Change logged information somewhat.

--- auth/auth.py
+++ auth/auth.py
@@ -1,133 +1,155 @@
-#! /usr/bin/env python2
-# -*- mode: Python; -*-
+#! /usr/bin/env python3
 
-# jpiitula@ling.helsinki.fi for making Korp authentication work in
-# FIN-CLARIN; HTTP Basic Authentication for development version, and
-# Martin's Shibboleth session for production use (trusting
-# REMOTE_USER, which korp.cgi passes as remote_user).
+"""
+auth.py
 
-# This server script is meant to receive HTTP Basic Authentication
-# credentials (username and password in plain) from Korp backend
-# (korp.cgi) and then return the list of authorized korpora in JSON (I
-# think).
+Flask WSGI application for the authorization server.
 
-# This script may be written to log some info in a file -- set
-# level=logging.INFO for that, or perhaps that code should now be
-# removed.
+This application is based on the older auth.cgi script written mainly
+by Jussi Piitulainen and Martin Matthiesen. Support for basic
+authentication was removed.
+"""
 
-from __future__ import print_function
 
-import cgi
-import json
-import MySQLdb
+# The following has been copied from korp.py, but not all that may be necessary
+# here, in particular if the script is run in a single thread.
 
-import logging
+# Skip monkey patching if run through gunicorn (which does the patching for us)
 import os
+if "gunicorn" not in os.environ.get("SERVER_SOFTWARE", ""):
+    from gevent import monkey
+    # Patching needs to be done as early as possible, before other imports
+    monkey.patch_all(subprocess=False)
+
+from gevent.pywsgi import WSGIServer
+from gevent.threadpool import ThreadPool
+from gevent.queue import Queue, Empty
+
+# gunicorn patches everything, and gevent's subprocess module can't be
+# used in native threads other than the main one, so we need to
+# un-patch the subprocess module.
+from importlib import reload
+import subprocess
+reload(subprocess)
+
 
 import sys
+import json
+import MySQLdb
+import logging
 
-import korp_config as config
+from flask import (Flask, request, Response, stream_with_context,
+                   copy_current_request_context)
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 
-sys.stderr = sys.stdout
+import config
 
-LOG_FILE = "/v/korp/log/korp-auth.log"
-LOG_LEVEL = logging.INFO    # in non-logging version, WARNING
 
-# academic is TRUE if 'member' is part of affiliation.
-# This is NOT true for member@clarin!
-# OR ACA status via LBR is set
-def is_academic(clarin,form):
-    aca_affiliation_values = ['member', 'employee', 'student', 'faculty', 'staff']
-    affiliation = form.get('affiliation', '').lower()
-    entitlement = form.get('entitlement', '')
+app = Flask(__name__)
+CORS(app)
 
+# Configure database connection
+app.config["MYSQL_HOST"] = config.DBHOST
+app.config["MYSQL_USER"] = config.DBUSER
+app.config["MYSQL_PASSWORD"] = config.DBPASSWORD
+app.config["MYSQL_DB"] = config.DBNAME
+app.config["MYSQL_PORT"] = config.DBPORT
+app.config["MYSQL_USE_UNICODE"] = True
+# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+mysql = MySQL(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def auth():
+    """Endpoint / returns permitted resources based on arguments.
+
+    Return permitted resources based on username (remote_user),
+    affiliation, and entitlement.
+    """
+    if request.is_json:
+        args = request.get_json()
+    else:
+        args = request.values.to_dict()
+    # Should the arguments and result be logged at the level info or debug?
+    logging.info("Arguments: %s", args)
+    result = _get_permitted_resources(
+        *(args.get(key, "") for key in [
+            "remote_user", "affiliation", "entitlement"]))
+    logging.info("Result: %s", result)
+    return Response(json.dumps(result), mimetype="application/json")
+
+
+def _is_academic(affiliation, entitlement, clarin):
+    """Return true if the user is academic based on the arguments."""
+    aca_affiliation_values = [
+        "member", "employee", "student", "faculty", "staff"]
     academic = (
-    (not clarin
-     and any(key in affiliation for key in aca_affiliation_values)
-    ) or
-    (clarin and
-     'http://www.clarin.eu/entitlement/academic' in entitlement
-    )
-    or
-    'urn:nbn:fi:lb-2016110710' in entitlement
+        # Non-CLARIN username and affiliation is one of the above
+        (not clarin
+         and any(key in affiliation for key in aca_affiliation_values)
+     ) or
+        # CLARIN username and academic
+        (clarin and
+         "http://www.clarin.eu/entitlement/academic" in entitlement
+     ) or
+        # Academic status via LBR
+        "urn:nbn:fi:lb-2016110710" in entitlement
     )
     return academic
 
 
-def main():
-    print_header()
+def _get_permitted_resources(username, affiliation, entitlement):
+    """Return permitted resources based on the arguments.
 
-    form_raw = cgi.FieldStorage(keep_blank_values=True)
-    form = dict((field, form_raw.getvalue(field))
-                for field in form_raw.keys())
-    logging.info('raw form: %s', form)
+    username is the value of REMOTE_USER, affiliation is from
+    Shibboleth and entitlement contains LBR REMS IDs (URNs) as a
+    semicolon separated list.
+    """
+    logging.info("Username: %s", username)
+    if not username:
+        return dict(authenticated=False)
 
-    conn = MySQLdb.connect(**config.AUTH_DBCONNECT)
-    cursor = conn.cursor()
+    cursor = mysql.connection.cursor()
 
-    authenticated, academic = False, False
-    top_domain = ''
-    entitlement = ''
-
-    if 'remote_user' in form:
-        username = form['remote_user']
-        clarin = username.endswith("@clarin.eu")
-        clarin_fi = username.endswith(".fi@clarin.eu")
-        # Get the top-level-domain for checking ACA-Fi
-        top_domain = username.rpartition('.')[-1]
-        authenticated = True
-        # entitlement contains LBR REMS IDs (URNs) as a semicolon separated list. 
-        entitlement = form['entitlement']
-        # convert entitle ment to a SQL friendly form
-        if entitlement:
-            entitlement = tuple(filter(None,(entitlement+';').split(';')))
-        else:
-            entitlement = tuple('')
-
-        # Determine "academic status" based on supplied attributes
-        academic = is_academic(clarin,form)
-        # set topdomain = fi if the user is academic and a CLARIN user with a fin. email
-        # The ACA status must have come from LBR in that case
-        if academic and clarin_fi:
-            top_domain='fi'
+    clarin = username.endswith("@clarin.eu")
+    clarin_fi = username.endswith(".fi@clarin.eu")
+    # Get the top-level-domain for checking ACA-Fi
+    top_domain = username.rpartition(".")[-1]
+    # Convert entitlement to an SQL-friendly form
+    if entitlement:
+        entitlement = tuple(filter(None, (entitlement + ";").split(";")))
     else:
-        username = form.get('username', '')
-        password = form.get('password', '')
-        # # Faking the ACA bit - not for production use:
-        # if username.endswith('+ACA'):
-        #     username, _ = username.rsplit('+')
-        #     academic = True
-        cursor.execute('''
-        select secret from auth_secret
-        where person = %s''', [username])
-        secret = cursor.fetchone()
-        if secret is not None and secret[0] == password:
-            authenticated = True
+        entitlement = tuple("")
 
-    logging.info('Is Academic: %s', academic)
-    logging.debug('DEBUG entitlement %s',entitlement)
+    # Determine "academic status" based on supplied attributes
+    academic = _is_academic(affiliation, entitlement, clarin)
+    # Set topdomain = fi if the user is academic and a CLARIN user
+    # with a .fi email.
+    # The ACA status must have come from LBR in that case
+    if academic and clarin_fi:
+        top_domain = "fi"
 
+    logging.debug("Is-Academic: %s", academic)
+    logging.debug("Entitlement: %s", entitlement)
 
-    # We can grant ACA status to people locally:
-    if  authenticated and not academic:
-        cursor.execute('''
-        select 1 from auth_academic
-        where person = %s''', [username])
+    # We can grant ACA status to people locally
+    if not academic:
+        cursor.execute("""
+            select 1 from auth_academic
+            where person = %s""", [username])
         if cursor.fetchone():
             academic = True
 
-    if authenticated:
+    # entitlement is a tuple of URNs that need mapping to Korp corpus IDs
+    # Create as many parameters as entitlement has entries. The empty
+    # list is "''"; eg 2 parameters yield ""%s", "%s""
+    in_parameters=", ".join(map(lambda x: "'%s'", entitlement))
+    if not in_parameters:
+        in_parameters = "''"
 
-        # entitlement is a tuple of URNs that need mapping to Korp corpus IDs
-       
-        # create as many parameters as entitlement has entries. The empty list is "''"
-        # eg 2 parameters yield "'%s', '%s'"
-        in_parameters=', '.join(map(lambda x: "'%s'", entitlement))
-        if not in_parameters:
-            in_parameters = "''"
-
-        # The query with parameters filled in. This is easier to debug.
-        sql='''
+    # The query with parameters filled in. This is easier to debug.
+    sql="""
         select corpus from auth_license
         where %s = True and (license = 'ACA'
                              or (license = 'ACA-Fi' and '%s' = 'fi'))
@@ -136,44 +158,37 @@ def main():
         where person = '%s'
         union distinct
         select corpus from auth_lbr_map
-        where lbr_id IN (%s); ''' % (academic, top_domain, username,
+        where lbr_id IN (%s); """ % (academic, top_domain, username,
                                      in_parameters)
 
-        # finally fill in entitlement values
-        sql = sql % entitlement
+    # Finally fill in entitlement values
+    sql = sql % entitlement
+    logging.debug("SQL: %s", sql)
 
-        logging.debug('DEBUG sql: %s',sql)
+    cursor.execute(sql)
+    corpora = [corpus.upper() for corpus, in cursor]
+    logging.info("Corpora: %s", corpora)
 
-        cursor.execute(sql)
+    return dict(authenticated=True,
+                permitted_resources=dict(username=username,
+                                         corpora=corpora))
 
-        corpora = [ corpus for corpus, in cursor ]
-        logging.debug('DEBUG corpora: %s',corpora)
 
-        result = dict(authenticated=True,
-                      permitted_resources=dict(username=username,
-                                               corpora=corpora))
+if __name__ == "__main__":
+    # Logging in this way may result in mixed lines if multiple instances of
+    # the auth application are run simultaneously. However, it might suffice to
+    # run a single instance at a time.
+    logging.basicConfig(
+        filename=config.LOG_FILE,
+        format=("[auth.py %(levelname)s %(process)d @ %(asctime)s] "
+                "%(message)s"),
+        level=config.LOG_LEVEL)
+    if len(sys.argv) == 2 and sys.argv[1] == "dev":
+        # Run using Flask (use only for development)
+        app.run(debug=True, threaded=True,
+                host=config.WSGI_HOST, port=config.WSGI_PORT)
     else:
-        result = dict(authenticated=False)
-
-    result = json.dumps(result)
-    logging.info('result: %s', result)
-    print(result)
-    conn.close()
-
-def print_header():
-    '''Copied from korp.cgi - may not be quite right here'''
-    print("Content-Type: application/json")
-    print("Access-Control-Allow-Origin: *")
-    print("Access-Control-Allow-Methods: GET, POST")
-    print("Access-Control-Allow-Headers: Authorization")
-    print()
-
-if __name__ == '__main__':
-    logging.basicConfig(filename=LOG_FILE,
-                        format=('[auth ' + str(os.getpid()) +
-                                ' @ %(asctime)s] %(message)s'),
-                        level=LOG_LEVEL)
-
-    logging.info('entering auth.cgi')
-    main()
-    logging.info('exiting auth.cgi')
+        # Run using gevent
+        print("Serving using gevent")
+        http = WSGIServer((config.WSGI_HOST, config.WSGI_PORT), app.wsgi_app)
+        http.serve_forever()
