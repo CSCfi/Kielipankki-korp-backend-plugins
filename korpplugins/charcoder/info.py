693ec3f85347414189469b97a8f81d837a250b7f
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-09-24 16:41:59 +0300

    korpplugins.charcoder: Decode chars in dict keys
    korpplugins/charcoder/__init__.py:
    - Also decode special characters in dict keys, so that the values of
      /struct_values with count=true are decoded correctly. (In the result
      of /struct_values?count=true, struct values are keys with frequencies
      as values.)

--- korpplugins/charcoder/info.py
+++ korpplugins/charcoder/info.py
@@ -7,7 +7,7 @@ Information on the charcoder Korp plugin.
 
 
 NAME = "Special character encoder/decoder"
-VERSION = "0.2"
-DATE = "2029-12-30"
+VERSION = "0.3"
+DATE = "2021-09-24"
 AUTHOR = "Jyrki Niemi / FIN-CLARIN"
 DESCRIPTION = "Encode special characters in queries and decode in results."
